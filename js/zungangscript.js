ea2f4c9694c880de25a161f8206c1d3760a7f9b2
userb3rt <rzajr868@gmail.com>
2024-08-21 12:05:57 +0200

    ordenación

---
+++ js/zungangscript.js
@@ -0,0 +1,22 @@
+// Obtener los elementos del DOM
+const modal = document.getElementById("loginModal");
+const zugangLink = document.querySelector("a[id='zunganglogin']");
+const closeModalBtn = document.getElementsByClassName("close")[0];
+
+// Abrir el modal cuando se hace clic en "Zugang"
+zugangLink.onclick = function (event) {
+  event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+  modal.style.display = "block";
+};
+
+// Cerrar el modal cuando se hace clic en la 'x'
+closeModalBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Cerrar el modal cuando se hace clic fuera de él
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
